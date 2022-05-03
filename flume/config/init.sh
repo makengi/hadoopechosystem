d3fa1eaea34ce17768a1b8e0908ac067ac2dc57f
cjy <cjy@imrbiz.co.kr>
2022-05-03 17:42:06 +0900

    updated flume avro client shell script

--- flume/config/init.sh
+++ flume/config/init.sh
@@ -2,7 +2,7 @@
 
 echo $configFile
 echo $agentName
-
+echo $avroHost
 echo $spoolDir
 
 equal_str="="
@@ -24,15 +24,30 @@ else
     #sed -i 's/${result_str}/${replace_path}/g' /opt/flume/conf/$configFile
 fi
 
+if [ -n $avroHost ]
+then
+    result_str=`sed -n "/${agentName}.sinks.AvroSink.hostname/p" /opt/flume/conf/$configFile`
+    split_str=(`echo $result_str | tr "=" ' '`)
+    echo ${split_str[1]}
+
+    # 대체할 아이피
+    replace_hostname=${split_str[0]}$equal_str$avroHost
+    echo "replace hostname: ${replace_hostname}"
+    sed -i "s~${result_str}~${replace_hostname}~g" /opt/flume/conf/$configFile
+fi    
+
+
 if [ -z $configFile ]
 then
     echo "Empty Config File"
 else
     echo "# Not Empty Config File"
     bash /opt/flume/bin/flume-ng agent -Xms1000m -Xmx2000m --conf conf --conf-file /opt/flume/conf/${configFile} --name ${agentName} -Dlog4j.configuration=file:/opt/flume/conf/log4j.properties
-fi    
+fi
 
 
+
+    
 
 # 로그 출력
 tail -f /opt/flume/logs/flume.log
